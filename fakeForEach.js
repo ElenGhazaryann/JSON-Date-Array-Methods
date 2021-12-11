fe84a365a4f363c6ed7d6cf52efb470f25b34710
Elen <elen.ghazaryan6@gmail.com>
2021-12-11 18:53:44 +0400

    feat: implementation for ForEach method

---
+++ fakeForEach.js
@@ -0,0 +1,9 @@
+function fakeForEach(arr, callback) {
+  return callback(arr);
+}
+
+fakeForEach([1, 2, 3, 4, 5], function (val) {
+  for (let i = 0; i < val.length; i++) {
+    console.log(val[i]);
+  }
+});
