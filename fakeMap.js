0c5822a9381d51ecdc3f9354a535a1ddcbf7ae9b
Elen <elen.ghazaryan6@gmail.com>
2021-12-22 02:10:51 +0400

    fix: fix map method

--- fakeMap.js
+++ fakeMap.js
@@ -1,16 +1,14 @@
-function myMap(arr, cb, index, array) {
+function myMap(arr, cb) {
   let newArr = [];
 
   for (let i = 0; i < arr.length; i++) {
-    index = i;
-    array = arr;
-    newArr.push(cb(arr[i], index, array));
+    newArr.push(cb(arr[i], i, arr));
   }
   return newArr;
 }
 
 console.log(
-  myMap([1, 2, 3], function (val, index, array) {
+  myMap([1, 2, 3], function (val, i, arr) {
     return val + 5;
   })
 );
